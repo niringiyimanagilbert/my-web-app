7cfaaceec53d7754939366b89722ee612c764f03
niringiyimanagilbert <niringiyimanagilbert20@gmail.com>
2025-10-08 09:59:39 +0200

    Initial commit: add basic Node.js app

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
+// FILE: my-web-app/index.js
 const http = require('http');
-const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  fs.readFile('index.html', (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      res.end('Error loading page');
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(data);
-    }
-  });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World! Server iriho. Murakaza neza!');
 });
 
 server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+  console.log('Server running at http://localhost:3000');
 });
+
