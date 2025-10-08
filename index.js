cd60666d1bb56667a6338c69564d0a940bd0a6c3
niringiyimanagilbert <niringiyimanagilbert20@gmail.com>
2025-10-08 06:00:30 +0200

    Initial commit: add basic Node.js app

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello developer");
+// FILE: my-web-app/index.js
+// RW: Aha ni ho ushyira code y'urusobe rwa server
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World! Server iriho. Murakaza neza!'); // mixed EN + RW message
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
