1d6a30dcf5f3c0aecae220e05e6d9a825479da5b
niringiyimanagilbert <niringiyimanagilbert20@gmail.com>
2025-10-08 06:50:02 +0200

    Initial commit: basic Node.js app

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-// FILE: my-web-app/index.js
-// RW: Aha ni ho ushyira code y'urusobe rwa server
-
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-// change the text in res.end:
-res.end('Hello! Ubu twavuguruye message — updated from local change.');
+  fs.readFile('index.html', (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Error loading page');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+  console.log('Server running on http://localhost:3000');
 });
